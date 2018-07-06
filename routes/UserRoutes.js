835e61a23b84214b3667c236bb6b5aafb36c68db
Gilbert Chan <unusgilbert@gmail.com>
2018-07-06 13:45:36 -0700

    Add route to deposit

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,4 +1,5 @@
 const User = require('./../models/User');
+const mongoose = require('mongoose');
 
 module.exports = function(app) {
     app.get('/api/users', (req, res) => {
@@ -35,6 +36,22 @@ module.exports = function(app) {
         user.save(function(err, user) {
             userId = user._id;
         });
+
+        res.sendStatus(200);
+    });
+
+    app.post('/api/users/deposit', (req, res) => {
+        var {
+            amountToAdd,
+            user
+        } = req.body;
+
+        user = mongoose.Types.ObjectId(bet);
+        var foundUser = User.findById();
+
+        foundUser.balance = foundUser.balance + amountToAdd;
+
+        foundUser.save();
 
         res.sendStatus(200);
     });
