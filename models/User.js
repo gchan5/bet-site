2f5ba0206c31d7b8781c706e42ac1066f4d8d04b
Gilbert Chan <unusgilbert@gmail.com>
2018-07-16 01:28:19 -0700

    Add pre-hook to hash user's password
    - installed bcrypt library

--- models/User.js
+++ models/User.js
@@ -1,9 +1,18 @@
+const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
 const UserSchema = new Schema({
-    username: String,
-    password: String,
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
     balance: Number,
     wins: Number,
     losses: Number,
@@ -11,6 +20,17 @@ const UserSchema = new Schema({
     ownedBets: [Schema.Types.ObjectId],
     oracledBets: [Schema.Types.ObjectId],
     pastBets: [Schema.Types.ObjectId]
+});
+
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            user.password = hash;
+            next();
+        });
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
