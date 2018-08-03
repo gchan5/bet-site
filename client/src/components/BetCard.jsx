2c4afac9001b3f817cad784c22c41af592b3cff4
Gilbert Chan <unusgilbert@gmail.com>
2018-08-02 19:26:59 -0700

    Add view for bets to home page
    - use bootstrap cards and flex styling

---
+++ client/src/components/BetCard.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class BetCard extends Component {
+    constructor(props) {
+        super(props);
+
+        const colors = ["danger", "success", "primary", "warning", "info"];
+
+        this.state = {
+            color: colors[Math.floor(Math.random() * (colors.length))]
+        }
+
+        console.log(this.state.color)
+    }
+
+    render() {
+        return (
+            <div className={"card text-white home-card border-" + this.state.color}>
+                <div className={"card-body text-" + this.state.color}>
+                    <h5 className="card-title">{this.props.name}</h5>
+                    <p className="card-text">{this.props.description}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default BetCard;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
 import { UserContext } from '../UserContext';
+
 import Navbar from './Navbar';
+import BetCard from './BetCard';
+import { getRequest} from '../utils/ApiUtils.js';
 import '../styles/App.css';
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bets: [],
+            shownBets: []
+        }
+
+        getRequest('/api/bet').then((response) => {
+            if(response.ok) {
+                response.json().then((responseJson) => {
+                    var bets = [];
+
+                    for(const bet in responseJson) {
+                        bets.push(responseJson[bet]);
+                    }
+
+                    this.setState({
+                        bets: bets,
+                        shownBets: bets
+                    });
+                })
+            }
+        });
+    }
+
     render() {
         return (
             <div className="App">
                 <UserContext.Consumer>
                     {({username, removeUser}) => <Navbar user={username} removeUser={removeUser} history={this.props.history} />}
                 </UserContext.Consumer>
-                <p className="App-intro">
-                    Home
-                </p>
+                <div className="container wrap-cards">
+                    {
+                        this.state.shownBets.map(function(bet, place) {
+                            return (
+                                <BetCard
+                                    key={place}
+                                    name={bet.name}
+                                    description={bet.description}
+                                />
+                            )
+                        })
+                    }
+                </div>
             </div>
         )
     }
